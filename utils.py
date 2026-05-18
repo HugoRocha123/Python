d54a6acebeb273ed21bb5a3e9cf90b7004b868bf
Hugo Daniel Soares Rocha <rochahugo206@gmail.com>
2026-05-18 15:32:15 +0100

    Delete utils.py

--- utils.py
+++
@@ -1,9 +0,0 @@
-def ImprimirCor(texto, opcao):
-    if opcao == 1:
-        print(f"\033[94m{texto}\033[00m")
-    elif opcao == 2:
-        print(f"\033[92m{texto}\033[00m")
-    elif opcao == 3:
-        print(f"\033[31m{texto}\033[00m")
-    else:
-        print(texto)
