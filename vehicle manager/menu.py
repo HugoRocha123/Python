6090e19d411ad0504fa445bdf09156532092b33a
Hugo Daniel Soares Rocha <rochahugo206@gmail.com>
2026-05-19 14:19:15 +0100

    Update menu.py

--- vehicle manager/menu.py
+++ vehicle manager/menu.py
@@ -4,9 +4,21 @@ from utils import ImprimirCor
 
 
 def obter_entrada_veiculo():
-    marca = input("Marca: ").strip()
-    modelo = input("Modelo: ").strip()
-    cor = input("Cor: ").strip()
+    while True:
+        tipo = input("Tipo de veículo: ").strip()
+        if tipo:
+            break
+        ImprimirCor("Erro: este campo não pode estar vazio.",3)
+    while True:
+        marca = input("Marca: ").strip()
+        if marca:
+            break
+        ImprimirCor("Erro: este campo não pode estar vazio.",3)
+    while True:
+        cor = input("Cor: ").strip()
+        if cor:
+            break
+        ImprimirCor("Erro: este campo não pode estar vazio.",3)
     while True:
         try:
             ano = int(input("Ano: "))
@@ -18,27 +30,32 @@ def obter_entrada_veiculo():
             ImprimirCor("Input inválido.",3)
         except Exception as erro:
             ImprimirCor(f"Um erro ocorreu no programa \n \"{erro}\"", 3)
-    return {"marca" : marca, "modelo" : modelo, "ano" : ano, "cor": cor}
+    return {"Marca" : marca, "Tipo de veículo" : tipo, "Ano" : ano, "Cor": cor}
 
 def executar_menu():
     gestor = GestorEstruturaJSON(r"C:\Users\trukz\Desktop\Programação\Aula2\Trabalho veículos\frota.json")
     while True:
-        ImprimirCor("="*30,1)
-        ImprimirCor("1 - Adicionar veículo.",1)
-        ImprimirCor("2 - Recuperar veículo.",1)
-        ImprimirCor("3 - Atualizar veículo.",1)
-        ImprimirCor("4 - Apagar um veículo",1)
-        ImprimirCor("0 - Sair do programa.",1)
-        ImprimirCor("="*30,1)
+        ImprimirCor("=" * 40, 1)
+        ImprimirCor("MENU PRINCIPAL".center(40), 1)
+        ImprimirCor("=" * 40, 1)
+        ImprimirCor("  1 - Adicionar veículo", 1)
+        ImprimirCor("  2 - Recuperar veículo", 1)
+        ImprimirCor("  3 - Atualizar veículo", 1)
+        ImprimirCor("  4 - Apagar um veículo", 1)
+        ImprimirCor("  0 - Sair do programa", 1)
+        ImprimirCor("=" * 40, 1)
+
         try:
             i = int(input('\033[96m Introduza uma opcao: \033[00m'))
             match i:
                     case 1:
                         while True:
-                            chave = input("Digite a matrícula: ").strip().upper()
-                            if not fullmatch(r"[A-Z0-9]{2}-[A-Z0-9]{2}-[A-Z0-9]{2}", chave):
-                                ImprimirCor("Matrícula inválida - Ex: \"AA-BB-11\"", 3)
+                            chave = input("Digite a matrícula: ").upper()
+                            if not fullmatch(r"(?:[A-Z0-9]{2}-[A-Z0-9]{2}-[A-Z0-9]{2}|[A-Z0-9]{6})", chave):
+                                ImprimirCor('Matrícula inválida - Ex: "AA-BB-11" ou "AABB11"', 3)
                                 continue
+                            chave = chave.replace("-", "")
+                            chave = f"{chave[:2]}-{chave[2:4]}-{chave[4:6]}"
                             if gestor.existe(chave):
                                 ImprimirCor("Essa matrícula já existe.", 3)
                                 continue
@@ -48,6 +65,7 @@ def executar_menu():
                             ImprimirCor("Veículo criado com sucesso",2)
                         else:
                             ImprimirCor("Um erro aconteceu.",3)
+                            
                     case 2:
                         chave = input("Digite a matrícula: ").strip().upper()
                         veiculo = gestor.recuperar(chave)
@@ -76,8 +94,9 @@ def executar_menu():
                         else:
                            ImprimirCor("A matrícula não foi encontrada",3)
                     case 0:
-                        ImprimirCor('\nSair do programa.',1)
-                        ImprimirCor("="*30,1)
+                        ImprimirCor("=" * 40, 1)
+                        ImprimirCor("A FECHAR O PROGRAMA".center(40), 1)
+                        ImprimirCor("=" * 40, 1)
                         break
                     case _:
                         print("Opção inválida.")
